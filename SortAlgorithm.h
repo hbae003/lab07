c93e5531ee51f4b8903acfeb03ef5f1cf743a9a6
Rebecca Hom <rhom001@hammer.cs.ucr.edu>
2015-11-09 14:41:47 -0800

    fixed SortAlgorithm's circular dependency

--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -1,32 +1,35 @@
-#ifndef SORTALGORITHM_H
-#define SORTALGORITHM_H
+#ifndef SORT_ALGORITHM_H
+#define SORT_ALGORITHM_H
 
-#include "Container.h"
+//  #include "Container.h"  -will cause circular dependency
 
+using namespace std;
+
+class Container;
 class SortAlgorithm
 {
- public: 
-    virtual void sort(Container* c) = 0;
+    public: 
+        virtual void sort(Container* c) = 0;
 };
 
 class BubbleSort: public SortAlgorithm
 {
- public:
-    void sort(Container* c);
+    public:
+        void sort(Container* c);
 };
 
 class SelectionSort: public SortAlgorithm 
 {
- public:
-    void sort(Container* c);
+    public:
+        void sort(Container* c);
 };
 
 class MergeSort: public SortAlgorithm
 {
- public:
-    void sort(Container* c);
-    void merge(Container *c, int low, int mid, int high);
-    void merge_sort(Container *c, int low, int high);
+    public:
+        void sort(Container* c);
+        void merge(Container *c, int low, int mid, int high);
+        void merge_sort(Container *c, int low, int high);
 };
 
 #endif
